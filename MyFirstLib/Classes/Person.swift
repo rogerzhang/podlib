73a983869dd3b08a5fe02321799f494ef7e65f05
Roger Zhang <roger.zhang@ringcentral.com>
2018-11-15 15:03:05 +0800

    110

---
+++ MyFirstLib/Classes/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  FBSnapshotTestCase
+//
+//  Created by Roger Zhang on 2018/11/13.
+//
+
+import Foundation
+
+public class Person: NSObject {
+    public var name = "name"
+    public var age = 12
+
+    public func drance() {
+        NSLog("Person", "drancing....")
+    }
+}
--- MyFirstLib_ringcentral.podspec
+++ MyFirstLib_ringcentral.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MyFirstLib_ringcentral'
-  s.version          = '1.0.4'
+  s.version          = '1.1.0'
   s.summary          = 'A short description of MyFirstLib.'
 
 # This description is used to generate tags and improve search results.
